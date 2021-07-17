5976c7fb4cc11ca45ecd8f5f3e31be74378d8313
Qian Luo <qianqianluo@gmail.com>
2021-07-17 02:26:45 -0700

    change for workflow and buildDocs.sh

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -17,7 +17,7 @@ set -x
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-stemmer python3-git python3-pip python3-virtualenv python3-setuptools
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-stemmer python3-git python3-pip python3-virtualenv 
  
 python3 -m pip install --upgrade rinohtype pygments
  
@@ -28,6 +28,10 @@ python3 -m pip install --upgrade rinohtype pygments
 pwd
 ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+# make a new temp dir which will be our GitHub Pages docroot
+docroot=`mktemp -d`
+ 
+export REPO_NAME="${GITHUB_REPOSITORY##*/}"
 
 ##############
 # BUILD DOCS #
@@ -35,9 +39,47 @@ export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
  
 # first, cleanup any old builds' static assets
 make -C docs clean
-make -C docs html
+# get a list of branches, excluding 'HEAD' and 'gh-pages'
+versions="`git for-each-ref '--format=%(refname:lstrip=-1)' refs/remotes/origin/ | grep -viE '^(HEAD|gh-pages)$'`"
+
+for current_version in ${versions}; do
+  
+   # make the current language available to conf.py
+   export current_version
+   git checkout ${current_version}
+  
+   echo "INFO: Building sites for ${current_version}"
+  
+   # skip this branch if it doesn't have our docs dir & sphinx config
+   if [ ! -e 'docs/conf.py' ]; then
+      echo -e "\tINFO: Couldn't find 'docs/conf.py' (skipped)"
+      continue
+   fi
+  
+   languages="en `find docs/locales/ -mindepth 1 -maxdepth 1 -type d -exec basename '{}' \;`"
+   for current_language in ${languages}; do
+  
+      # make the current language available to conf.py
+      export current_language
+      ##########
+      # BUILDS #
+      ##########
+      echo "INFO: Building for ${current_language}"
+  
+      # HTML #
+      sphinx-build -b html docs/ docs/build/html/${current_language}/${current_version} -D language="${current_language}"
  
-www.g
+  
+      # copy the static assets produced by the above build into our docroot
+      rsync -av "docs/build/html/" "${docroot}/"
+  
+   done
+  
+done
+
+# return to master branch
+git checkout master
+
 #######################
 # Update GitHub Pages #
 #######################
@@ -60,6 +102,20 @@ git checkout -b gh-pages
 # add .nojekyll to the root so that github won't 404 on content added to dirs
 # that start with an underscore (_), such as our "_content" dir..
 touch .nojekyll
+
+# add redirect from the docroot to our default docs language/version
+cat > index.html <<EOF
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>helloWorld Docs</title>
+      <meta http-equiv = "refresh" content="0; url='/${REPO_NAME}/en/master/'" />
+   </head>
+   <body>
+      <p>Please wait while you're redirected to our <a href="/${REPO_NAME}/en/master/">documentation</a>.</p>
+   </body>
+</html>
+EOF
 
 # copy the resulting html pages built from sphinx above to our new git repo
 git add .
